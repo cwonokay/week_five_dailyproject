1f5f33ea0c9799982ce68a76f2111d0b49696c3c
cwonokay <cwonokay@gmail.com>
2017-08-21 11:39:54 -0400

    final

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const moment = require("moment");
 const chalk = require("chalk");
 
-    console.log("It is" +" "+ chalk.blue(moment().format('LLLL')) + " .");
-    console.log("It is" + " "+ chalk.green(moment().format('DDD'))+" " + "day of the year.");
+let hour         = 60 * 60 * moment().format("H");
+let minute       = 60 * moment().format("m");
+let second       = moment().format("s");
+let totalSeconds = hour + minute + parseInt(second);
 
-
+console.log("It is" +" "+ chalk.blue(moment().format('LLLL')) + " .");
+console.log("It is" + " "+ chalk.green(moment().format('DDD'))+" " + "day of the year.");
+console.log("It is" + " "+ chalk.red(totalSeconds)+" " + "into the day." );
+if (moment().isDST()=== true) {
 console.log("It is" + " " + chalk.yellow( moment([2017, 8, 14]).isDST()) + " " + " August 14, 2017 is during Daylight Savings Time.");
-console.log("It is" + " " + chalk.red( moment([2001]).isLeapYear()) + " " + " August 14, 2017 is not a leap year.");
+}else {
+    console.log("It is" + " " + chalk.yellow( moment([2017, 8, 14]).isDST()) + " " + " August 14, 2017 is during Daylight Savings Time.");
+  }
+if (moment().isLeapYear()=== false) {
+  console.log("It is" + " " + chalk.red( moment([2001]).isLeapYear()) + " " + " August 14, 2017 is not a leap year.");
+}else {
+  console.log("It is" + " " + chalk.red( moment([2001]).isLeapYear()) + " " + " August 14, 2017 is not a leap year.");
+}
